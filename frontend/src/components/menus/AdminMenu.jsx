cf0a9698d516fac0d13caa5b7df0ec05e1f7924a
JVKE <shubhambrown1@gmail.com>
2025-09-15 18:37:09 +0530

    Adding Logout funtunality

--- frontend/src/components/menus/AdminMenu.jsx
+++ frontend/src/components/menus/AdminMenu.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+
+import toast from "react-hot-toast";
+import Button from "../../components/button/Button";
 
 const AdminMenu = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Remove token and user info
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+    // Show success toast
+    toast.success("Logout successfully!");
+
+    navigate("/login");
+  };
+
   return (
     <nav className="flex flex-col space-y-4 p-4">
       <NavLink to="/dashboard/admin" className="dashboardMenuItems">
@@ -16,6 +32,12 @@ const AdminMenu = () => {
       <NavLink to="/dashboard/admin/reviews" className="dashboardMenuItems">
         Reviews
       </NavLink>
+
+      <Button
+        onClick={handleLogout}
+        text="Logout"
+        className="bg-red-600 hover:bg-red-700 text-white text-[18px]"
+      />
     </nav>
   );
 };
